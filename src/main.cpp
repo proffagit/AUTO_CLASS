f259582b9f133a3bc6a5b7fdb24417977496c2fc
Obed <e2101936@edu.vamk.fi>
2024-10-01 14:48:55 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "../lib/sqlite3/sqlite3.h"
+
+
+int main() {
+
+    // PRINTING HELLO WORLD
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
+
+
+
+
